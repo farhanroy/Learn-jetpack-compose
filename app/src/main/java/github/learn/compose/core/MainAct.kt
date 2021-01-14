cb55bab16e230ea76e7c94c221a1225fce49bf77
farhanroy <farhanroy120@gmail.com>
2021-01-14 22:37:53 +0700

    show image in jetpack compose

--- app/src/main/java/github/learn/compose/core/MainAct.kt
+++ app/src/main/java/github/learn/compose/core/MainAct.kt
@@ -11,6 +11,8 @@ class MainAct : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toAlertDialog()
     }
 
     fun toSimpleText() {
